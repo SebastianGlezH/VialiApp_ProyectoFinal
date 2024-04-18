09d31627044802ac1a806345ded7d4a10a74a988
UTLJun <81024@alumnos.utleon.edu.mx>
2024-04-17 22:28:10 -0600

    ACTUALIZACION -JUN-SI

---
+++ CalzaAR-App/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVRsT3N6Ny8vLy8vQUFBQm1YaVZ0ZmZycTB2d2szbHpobUpWOVhnR2RmdkRkdzNlNHBkUWlJeFVRNFFFMHM3dERwYk5SYzc5VnR1YW0wMzhNV1FVeVVWQkdhUWV4YzdHWkEzdWhjU3NyYVFZSDlaL2ExRGR1a0FHQ1dWRURmcXJoK1hCT1NKVUswUW9NWnlJVjM5SjNvdHlab2hEckl6ZE9STkRNaWxDVVgrdHhxNkYyOVJKLzZjaVZJSGpRUTIwckNqNlhkR0RsbS9teUhxQXUrenI0OG1hWlkvWDN6TGJxTTRQLy9kUVpVTFo1ek1xK3NpTnNLMjd5dndXS3dTSFMyQWV2SVRyVGhxQnNsNkJlUHA2VUQ4aWdUR0hET2doSS9RSlNuS1FtTXhodmU3YkJpZjVpTDdwcjk1TGJqSWlrbUlkSkEycmhZOEdhRW9CLyszRUJvOHl6ZGRydGJhaTVENGN4QUNqRUFQTlpvYmNYMWMydHdWdVExdHM=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
